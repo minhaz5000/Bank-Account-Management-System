72678aa3e70ba3d69a1b38aff24f0c0c2f439ee8
Minhazul Haque <minhazul@iut-dhaka.edu>
2023-03-05 11:39:22 +0600

    Add DBConnection Class for PostgreSQL DB Connection

--- Bank_Account_Management_System/src/main/java/module-info.java
+++ Bank_Account_Management_System/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.fma.bank_account_management_system {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
 
     opens com.fma.bank_account_management_system to javafx.fxml;
     exports com.fma.bank_account_management_system;
